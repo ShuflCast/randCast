f232265af2f152b5dfcf4d84894756f0728d0e9a
Toby Clarke <teeohbee@gmail.com>
2015-09-23 11:17:23 +0100

    Added controllers

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -25,16 +25,19 @@ angular.module('starter', ['ionic'])
     .state('home', {
     url: '/',
     templateUrl: 'templates/home.html',
+    controller: 'homeCtrl'
   })
 
   .state('results', {
     url: '/results',
     templateUrl: 'templates/results.html',
+    controller: 'resultsCtrl'
   })
 
   .state('player', {
     url: '/player',
     templateUrl: 'templates/player.html',
+    controller: 'playerCtrl'
   })
 
   $urlRouterProvider.otherwise('/')
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,1 +1,16 @@
 angular.module('starter.controllers', ['ionic'])
+
+.controller('homeCtrl', function($scope) {
+
+
+})
+
+.controller('resultsCtrl', function($scope) {
+
+
+})
+
+.controller('playerCtrl', function($scope) {
+
+
+})
