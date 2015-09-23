9aa7dbd2cc70d469c54897d5d0d73c2eb82767b2
Toby Clarke <teeohbee@gmail.com>
2015-09-23 10:53:21 +0100

    Added index routing

--- www/js/app.js
+++ www/js/app.js
@@ -9,11 +9,23 @@ angular.module('starter', ['ionic'])
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+    .state('home', {
+    url: '/',
+    templateUrl: 'templates/home.html',
+  })
+
+  $urlRouterProvider.otherwise('/')
+});
