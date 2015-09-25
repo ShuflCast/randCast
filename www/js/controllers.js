1ac4bd1c8148f2bb8674cd7c7c7fd86e5c8c54a3
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-09-25 18:12:27 +0100

    player singke oain working, description fixed to 2 lines, second converted however not quite reading how we want

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1167,7 +1167,8 @@ $scope.podcasts = $scope.json[0].results
     url: "http://robbbenson.com/01%20Angel.mp3",
     artist: "timmy mallet",
     title: "wack aday",
-    art: "http://images.askmen.com/top_10/celebrity/breakfast-tv-presenters_423192.jpg"
+    art: "http://images.askmen.com/top_10/celebrity/breakfast-tv-presenters_423192.jpg",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
   }
 
 })
