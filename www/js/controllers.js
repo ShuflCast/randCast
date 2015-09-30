b0c2a23d69df7ed6beb880fa71f9f5efec5246ef
Toby Clarke <teeohbee@gmail.com>
2015-09-30 15:37:08 +0100

    updated duration icons, time now in minutes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -107,3 +107,9 @@ app.filter('durationFilter', function(ApiCall) {
    return filtered;
  };
 });
+
+app.filter('secondsToDateTime', [function() {
+    return function(seconds) {
+        return new Date(1970, 0, 1).setSeconds(seconds);
+    };
+}])
